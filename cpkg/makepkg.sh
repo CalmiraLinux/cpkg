c54785ddeb72eb4998bd4753da8a6d52a27be6bf
Linuxoid85 <71516578+Linuxoid85@users.noreply.github.com>
2021-06-12 14:36:48 +0300

    Update makepkg.sh
    Добавлено:
    * информация о пакете берётся из конфига
    * просмотр содержимого пакета
    * удаление временных файлов
    * информация о создании директорий и копировании файлов теперь отправляется в лог, а не на экран

--- cpkg/makepkg.sh
+++ cpkg/makepkg.sh
@@ -7,6 +7,10 @@
 # Michail Krasnov linuxoid85@gmail.com
 #
 
+source config.sh
+PACKAGE="$NAME-$VERSION.txz"
+
+echo -e "\e[1mDIRS:\e[0m"
 for FILE in "usr" "usr/bin" "usr/lib" "usr/lib/cpkg" "etc" "etc/cpkg"; do
     if test -d $FILE; then
         echo "$FILE is found"
@@ -16,6 +20,7 @@ for FILE in "usr" "usr/bin" "usr/lib" "usr/lib/cpkg" "etc" "etc/cpkg"; do
     fi
 done
 
+echo -e "\n\e[1mFILES:\e[0m"
 for FILE in "usr/bin/cpkg" "usr/lib/cpkg/core-functions.sh" "usr/lib/cpkg/other-functions.sh" "etc/cpkg/settings"; do
     if test -f $FILE; then
         echo "$FILE is found"
@@ -25,40 +30,40 @@ for FILE in "usr/bin/cpkg" "usr/lib/cpkg/core-functions.sh" "usr/lib/cpkg/other-
     fi
 done
 
-mkdir -pv PKG/pkg
-cp -rv {usr,etc} PKG/pkg/
+echo -e "\nMake dirs and copy package data..."
+mkdir -pv PKG/pkg > log
+cp -rv {usr,etc} PKG/pkg/ >> log
 
-cat > PKG/config.sh << "EOF"
-NAME=cpkg
-VERSION=1.0pa3
-DESCRIPTION="CPkg - an automated packaging tool fog Calmira Linux"
-MAINTAINER=Linuxoid85
-FILES="/usr/bin/cpkg /usr/lib/cpkg /etc/cpkg/{pkg.list,settings}"
-EOF
+echo -e "Write package information..."
+echo "$(cat config.sh)" > PKG/config.sh
 
-cat > PKG/changelog << "EOF"
-** version 1.0pa3 ** 12.05.2021
-Prealpha 3.
- * Повышение стабильности пакетного менеджера
- * Исправлен баг с ошибкой занесения port-пакета в базу данных `cpkg`
- * Другие незначительные фиксы
+for FILE in "port.sh" "preinst.sh" "postinst.sh"; do
+    if test -f $FILE; then
+        echo "$FILE is found"
+        echo "$(cat $FILE)" > PKG/$FILE
+    else
+        echo "$FILE doesn't find in $PWD !"
+    fi
+done
 
-** version 1.0pa2 ** 12.05.2021
-Prealpha 2.
- * улучшен алгоритм скачивания пакетов
- * исправлен баг с выходом из cpkg при автоматическом скачивании пакета в процедуре установки
- * исправлено два бага с port-пакетами (#3 и #1)
- * добавлены опции для скачивания как одного пакета, так и всех пакетов в репозитории в целом
- * всяческие незначительные фиксы и исправления
+if test -f changelog; then
+    echo "changolog is found"
+    echo "$(cat changelog)" > PKG/changelog
+else
+    echo "changelog doesn't find in $PWD !"
+fi
 
-** version 1.0pa1 **  10.05.2021
-Prealpha 1. Initial Release
-EOF
+echo -e "\nBuild package..."
+tar -cf $PACKAGE PKG -J
 
-tar -cvf cpkg-1.0pa3.txz PKG -J
-
+echo "Test package..."
 if test -f "cpkg-1.0pa3.txz"; then
     echo "Build package done"
+    read -p "Show the package data? (Y/n) " run
+    if [ $run = Y ]; then
+        tar -listf $PACKAGE
+    fi
+    rm -rf PKG
 else
     echo "Build package FAIL"
 fi
