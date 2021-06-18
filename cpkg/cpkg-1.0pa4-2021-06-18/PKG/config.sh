4a1260453c94b66504d0370048a2fb21940dc97f
Linuxoid85 <linuxoid85@gmail.com>
2021-06-18 15:29:27 +0300

    Переименован cpkg на src

--- cpkg/cpkg-1.0pa4-2021-06-18/PKG/config.sh
+++
@@ -1,6 +0,0 @@
-NAME=cpkg
-VERSION=1.0pa4
-DESCRIPTION="CPkg - автоматизированная утилита работы с пакетами для Calmira GNU/Linux"
-REQ_DEPS="bash"
-MAINTAINER=Linuxoid85
-FILES="/usr/bin/cpkg /usr/share/cpkg /etc/cpkg /var/cache/cpkg"
--- cpkg/cpkg-1.0pa4-2021-06-18/PKG/pkg/var/db/cpkg/packages/system/preinst.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Start installing the system..."
