914f4c05680dcd7ba84ad9c0e230463df33bdaec
Linuxoid85 <linuxoid85@gmail.com>
2021-06-24 05:11:52 +0300

    Добавление функции 'GetCalmiraVersion' для отображения версии дистрибутива

--- src/usr/lib/cpkg/core-functions.sh
+++ src/usr/lib/cpkg/core-functions.sh
@@ -379,9 +379,9 @@ function edit_src() {
 function help_pkg() {
 	echo -e "\e[1;35m$CPKG_ABOUT\e[0m
 \e[1m$CPKG_VER\e[0m        $VERSION (PreAlpha 4)
-\e[1m$CPKG_DISTRO_VER\e[0m $GetCalmiraVersion
+\e[1m$CPKG_DISTRO_VER\e[0m $(GetCalmiraVersion)
 
-$HELP_CPKG $GetCalmiraVersion
+$HELP_CPKG $(GetCalmiraVersion)
 "
 }
 
